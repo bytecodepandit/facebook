6109636a876c0b68e34851ab85f3f6f1fe60355f
Prakash Kumar Mandal <prakash.mandal@binaryinformatics.com>
2021-04-03 10:31:36 +0530

    static data implementation done

--- lib/core/mocks/facebook_group_post.dart
+++ lib/core/mocks/facebook_group_post.dart
@@ -8,7 +8,7 @@ class FacebookGroupPostData {
     "postType": "FACEBOOK_GROUP_POST",
     "postTimeLong": "4 hrs",
     "user": {
-      "imgUrl": "https://scontent-del1-1.xx.fbcdn.net/v/t1.0-1/cp0/p60x60/105740287_2977827399102860_7342773435651660519_n.jpg?_nc_cat=1&_nc_sid=1eb0c7&_nc_ohc=-LoS3aq9Tv8AX-36xQo&_nc_oc=AQnVj7vXFt4t9vLM6x4LVQ33NW0TcwmfSQrttpoN_yg-K5pDFtpofm6piR-tMoBF7v0&_nc_ht=scontent-del1-1.xx&oh=a7a76c2eb3d6e8485cac42de89180087&oe=5F56DA24",
+      "imgUrl": "http://192.168.100.7:4100/assets/users/user-1.jpeg",
       "name": "Vishal Chaubey"
     },
     "likeCommentStatus": {
@@ -19,13 +19,13 @@ class FacebookGroupPostData {
   }, {
     "id": "2",
     "groupName": "Milla Kya-मिला क्या?",
-    "postContent": "https://scontent-del1-1.xx.fbcdn.net/v/t1.0-0/p526x296/117326426_3146145995474351_1822613753638760295_n.jpg?_nc_cat=107&_nc_sid=8bfeb9&_nc_ohc=Gn-LnuYWP6UAX9Y4pUi&_nc_ht=scontent-del1-1.xx&_nc_tp=6&oh=bd0673c2ec958744a43ca357e6dbc495&oe=5F575E22",
+    "postContent": "http://192.168.100.7:4100/assets/posts/post-1.jpeg",
     "contentDescription": "What is your thoughts on this??? Let us connect with each other and make this group for learning, sharing, discussion, fun... This is not my group. It is our group. You are free to share anything on this page. Share your stories, experience or anything you want to share. If you want to write for us, you can also do it for us. We will feature you on ",
     "mainContentType": "IMAGE",
     "postType": "FACEBOOK_PAGE_POST",
     "postTimeLong": "3 min",
     "user": {
-      "imgUrl": "https://scontent-del1-1.xx.fbcdn.net/v/t1.0-1/cp0/p60x60/18342093_1775843549302952_9168222940529941012_n.jpg?_nc_cat=103&_nc_sid=1eb0c7&_nc_ohc=RQjnQ_E7TCQAX-0HvTY&_nc_ht=scontent-del1-1.xx&oh=68a82ecbcd8f75fa41fd766f2fce9983&oe=5F541BAB",
+      "imgUrl": "http://192.168.100.7:4100/assets/users/user-2.jpeg",
       "name": "Saurabh Shukla"
     },
     "likeCommentStatus": {
@@ -33,16 +33,17 @@ class FacebookGroupPostData {
       "comments": "10k",
       "shares": "12k"
     }
-  }, {
+  }, 
+  {
     "id": "3",
     "groupName": "",
-    "postContent": "https://scontent-del1-1.xx.fbcdn.net/v/t1.0-0/p526x296/106457857_1414510812071510_942664082687708803_n.jpg?_nc_cat=102&_nc_sid=8bfeb9&_nc_ohc=wFBM_38YCHkAX8G35eP&_nc_ht=scontent-del1-1.xx&_nc_tp=6&oh=8534364f10ff2f24b37b7ab928eedc1d&oe=5F549DE6",
+    "postContent": "http://192.168.100.7:4100/assets/posts/post-2.jpeg",
     "contentDescription": "Be simple and curious 😋🤪🤪🙅",
     "mainContentType": "IMAGE",
     "postType": "FACEBOOK_USER_POST",
     "postTimeLong": "3 min",
     "user": {
-      "imgUrl": "https://scontent-del1-1.xx.fbcdn.net/v/t1.0-1/cp0/p40x40/109817474_1431435583712366_2875659392184942405_n.jpg?_nc_cat=101&_nc_sid=7206a8&_nc_ohc=TuH7Fn00gFcAX_hxu03&_nc_ht=scontent-del1-1.xx&oh=5e94a23e65d5730f46a654233d295f0c&oe=5F576412",
+      "imgUrl": "http://192.168.100.7:4100/assets/users/user-3.jpeg",
       "name": "Prakash Mandal"
     },
     "likeCommentStatus": {
@@ -50,5 +51,42 @@ class FacebookGroupPostData {
       "comments": "100",
       "shares": "10"
     }
-  }];
+  },
+  {
+    "id": "3",
+    "groupName": "",
+    "postContent": "http://192.168.100.7:4100/assets/posts/post-3.jpeg",
+    "contentDescription": "Be simple and curious 😋🤪🤪🙅",
+    "mainContentType": "IMAGE",
+    "postType": "FACEBOOK_USER_POST",
+    "postTimeLong": "4 min",
+    "user": {
+      "imgUrl": "http://192.168.100.7:4100/assets/users/user-4.jpeg",
+      "name": "Rahul Kumar"
+    },
+    "likeCommentStatus": {
+      "likes": "200k",
+      "comments": "100",
+      "shares": "10"
+    }
+  },
+  {
+    "id": "3",
+    "groupName": "",
+    "postContent": "http://192.168.100.7:4100/assets/posts/post-4.jpeg",
+    "contentDescription": "Be simple and curious 😋🤪🤪🙅",
+    "mainContentType": "IMAGE",
+    "postType": "FACEBOOK_USER_POST",
+    "postTimeLong": "3 min",
+    "user": {
+      "imgUrl": "http://192.168.100.7:4100/assets/users/user-9.jpeg",
+      "name": "Deepak Singh"
+    },
+    "likeCommentStatus": {
+      "likes": "600k",
+      "comments": "100",
+      "shares": "10"
+    }
+  }
+  ];
 }
